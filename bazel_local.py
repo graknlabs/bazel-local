c21fc3c4dd7bbdca46f0e32ecbdbefc72534c025
Adam Mitchell <adam@grakn.ai>
2020-07-06 11:26:02 +0100

    Various fixes

--- bazel_local.py
+++ bazel_local.py
@@ -65,7 +65,7 @@ def resolve_bazel_target(target):
     
     return target
 
-def create_cached_dep(repo, binary_apath):
+def create_cached_file_dep(cwd, repo, binary_apath, args):
     deps_cache = os.path.abspath(local_deps_dir)
     mkdir(deps_cache)
 
@@ -93,10 +93,29 @@ def create_cached_dep(repo, binary_apath):
 
     return repo_cache_apath
 
-def transitive_local(dir, local_repositories = {}):
-    dir = os.path.abspath(dir)
+def create_cached_archive_dep(cwd, repo, binary_apath, args):
+    deps_cache = os.path.abspath(local_deps_dir)
+    mkdir(deps_cache)
+
+    repo_cache_apath = os.path.join(deps_cache, repo)
+    mkdir(repo_cache_apath)
+
+    binary_name = os.path.basename(binary_apath)
+
+    subprocess.run(['tar', '-xzf', binary_apath, '-C', repo_cache_apath], check=True)
+
+    f = open(os.path.join(repo_cache_apath, 'WORKSPACE'), 'w')
+    f.write(f'workspace(name = "{repo}")\n')
+    f.close()
+
+    shutil.copyfile(os.path.join(cwd, args[0]), os.path.join(repo_cache_apath, 'BUILD'))
+
+    return repo_cache_apath
+
+def transitive_local(cwd, local_repositories = {}):
+    cwd = os.path.abspath(cwd)
     try:
-        local_deps = toml.load(os.path.join(dir, local_deps_file))
+        local_deps = toml.load(os.path.join(cwd, local_deps_file))
         local_deps_repos = {}
         try:
             local_deps_repos = local_deps['local_repositories']
@@ -109,25 +128,43 @@ def transitive_local(dir, local_repositories = {}):
                 return
             except KeyError:
                 pass
-            local_repositories[repo] = os.path.abspath(os.path.join(dir, path))
+            local_repositories[repo] = os.path.abspath(os.path.join(cwd, path))
             transitive_local(path, local_repositories)
     except FileNotFoundError:
         pass
 
-def bazel_tool(argv, dir, capture_output=False, check=False):
+def create_local_deps(cwd, repo, args, create_function):
+    path = args[0]
+    target = args[1]
+    apath = os.path.abspath(path)
+
+    target = resolve_bazel_target(target)
+
+    repo_target = f'@{repo}{target}'
+    bazel_tool(['build', target], apath, check=True)
+    dep_aquery = bazel_tool(['aquery', '--output', 'textproto', target], apath, capture_output=True, check=True).stdout.decode(sys.stdout.encoding)
+
+    binary_subpath = re.findall('exec_path: "(.*)"', dep_aquery)[-1]
+    binary_apath = os.path.join(apath, binary_subpath)
+
+    repo_cache_apath = create_function(cwd, repo, binary_apath, args[2:])
+
+    return [f'{repo}={repo_cache_apath}']
+
+def bazel_tool(argv, cwd, capture_output=False, check=False):
     command_pos = 0
     for arg in argv:
         if (arg == '--'):
-            return subprocess.run([unwrapped_bazel_command, *argv], check=check, capture_output=capture_output, cwd=dir)
+            return subprocess.run([unwrapped_bazel_command, *argv], check=check, capture_output=capture_output, cwd=cwd)
         if arg in bazel_commands:
             break
         command_pos += 1
     
     if command_pos == len(argv):
-        return subprocess.run([unwrapped_bazel_command, *argv], check=check, capture_output=capture_output, cwd=dir)
+        return subprocess.run([unwrapped_bazel_command, *argv], check=check, capture_output=capture_output, cwd=cwd)
 
     local_repositories = {}
-    transitive_local(dir, local_repositories)
+    transitive_local(cwd, local_repositories)
     
     local_repositories_args = []
     for repo, path in local_repositories.items():
@@ -139,7 +176,7 @@ def bazel_tool(argv, dir, capture_output=False, check=False):
     # Load local http_files
     http_files = {}
     try:
-        local_deps = toml.load(os.path.join(dir, local_deps_file))
+        local_deps = toml.load(os.path.join(cwd, local_deps_file))
         http_files = local_deps['http_files']
     except FileNotFoundError:
         pass
@@ -147,33 +184,33 @@ def bazel_tool(argv, dir, capture_output=False, check=False):
         pass
 
     http_files_args = []
-    for repo, path_target in http_files.items():
-        path = path_target[0]
-        target = path_target[1]
-        apath = os.path.abspath(path)
+    for repo, args in http_files.items():
+        for l in create_local_deps(cwd, repo, args, create_cached_file_dep):
+            http_files_args.append('--override_repository')
+            http_files_args.append(l)
+    
+    http_archives = {}
+    try:
+        local_deps = toml.load(os.path.join(cwd, local_deps_file))
+        http_archives = local_deps['http_archives']
+    except FileNotFoundError:
+        pass
+    except KeyError:
+        pass
 
-        target = resolve_bazel_target(target)
-
-        # subprocess.run(['bazel_tool', 'build', target], check=True, cwd=apath)
-        repo_target = f'@{repo}{target}'
-        bazel_tool(['build', target], apath, check=True)
-        dep_aquery = bazel_tool(['aquery', '--output', 'textproto', target], apath, capture_output=True, check=True).stdout.decode(sys.stdout.encoding)
-
-        binary_subpath = re.findall('exec_path: "(.*)"', dep_aquery)[-1]
-        binary_apath = os.path.join(apath, binary_subpath)
-
-        repo_cache_apath = create_cached_dep(repo, binary_apath)
-
-        http_files_args.append('--override_repository')
-        http_files_args.append(f'{repo}={repo_cache_apath}')
+    http_archive_args = []
+    for repo, args in http_archives.items():
+        for l in create_local_deps(cwd, repo, args, create_cached_archive_dep):
+            http_files_args.append('--override_repository')
+            http_files_args.append(l)
 
     command_args = [unwrapped_bazel_command, *argv[0:command_pos+1], *local_repositories_args, *http_files_args]
     if command_pos < len(argv) - 1:
         command_args.extend(argv[command_pos+1:])
 
     full_command = ' '.join(command_args)
-    print(dir, full_command)
-    return subprocess.run(command_args, cwd=dir, check=check, capture_output=capture_output)
+    print(cwd, full_command)
+    return subprocess.run(command_args, cwd=cwd, check=check, capture_output=capture_output)
 
 
 if __name__ == "__main__":
